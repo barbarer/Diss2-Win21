a4526021fc02d73075289877a0e8b491e04f72f4
Barbara Ericson <43884467+barbarer@users.noreply.github.com>
2021-02-01 12:19:24 -0500

    Add files via upload

---
+++ flower.py
@@ -0,0 +1,64 @@
+from turtle import *
+
+
+def drawPetal(turtle, width):
+    """
+    Write a function to draw the petal shape for the flower head.  It
+    can be a square, triangle, etc.  
+    """
+    pass
+
+
+def drawHead(turtle):
+    """
+    Write a function to draw a spirograph like pattern as the flower head.
+    It should call drawPetal in a loop and turn after each call to form a circle
+
+    :param turtle: An instance of the Turtle class
+    """
+    pass
+    
+
+
+def drawStem(turtle, xpos, ypos, width, length):
+    """
+    Write a function to draw a stem on the screen using the specified parameters.
+
+    :param turtle: An instance of the Turtle class
+    :param xpos: The X-axis coordinate for the starting point of the Stem
+    :param ypos: The Y-axis coordinate for the starting point of the Stem
+    :param width: The width of a side of the Stem
+    :param length: The length of a side of the Stem
+    :param color: The line color and fill color to use for the Stem
+    """
+    pass
+
+
+
+def draw_flower(turtle):
+    """
+    Write a function to draw a flower.  Draw the head and stem.
+
+    :param turtle: the turtle to draw with
+    """
+    pass
+
+def main():
+    """
+    Write a function that will be called when you run this program
+    from the terminal.
+
+    Make sure to create a Screen object, a Turtle object,
+    and call draw_flower. 
+
+    Also, make sure to call the .exitonclick() method on your Screen instance
+    to stop the program from exiting untill you close the drawing window.
+    """
+    pass
+
+
+# Only run the main function if this file is executed (not imported)
+if __name__ == '__main__':
+    main()
+
+
